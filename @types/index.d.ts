19fd6e282b72bb7ed1c9bea26b38643f4657bad0
besuper <16976439+besuper@users.noreply.github.com>
2023-07-21 18:31:01 +0200

    Fix types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -4,9 +4,9 @@ export class TwitterClient {
     authorization: string;
     auth_token: string;
 
-    async connect(): Promise<void>;
-    async like(tweet_id: string): Promise<void>;
-    async retweet(tweet_id: string): Promise<void>;
-    async follow(user_id: string): Promise<void>;
-    async search(query: string, type: string = "Latest", count: number = 20): Promise<any[]>;
+    connect(): Promise<void>;
+    like(tweet_id: string): Promise<void>;
+    retweet(tweet_id: string): Promise<void>;
+    follow(user_id: string): Promise<void>;
+    search(query: string, type?: string, count?: number): Promise<any[]>;
 }
